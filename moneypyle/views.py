1f2afec3560dc028bdac6b1fe5705260840e8700
herbsplit <boecker.sullivan@gmail.com>
2026-02-13 22:54:47 -0500

    got the form to send info properly. now I need to get the names to show up instead of just the object

--- moneypyle/views.py
+++ moneypyle/views.py
@@ -86,22 +86,27 @@ def new_entry(request):
     if request.method == "POST":
         form = TransactionForm(request.POST or None, prefix="trans")
         form_2 = EntryFormSet(request.POST or None, prefix="entries")
-
-
- 
-        if all([form.is_valid(), form_2.is_valid()]):
+        if form.is_valid() and form_2.is_valid():
             with transaction.atomic():
-                parent = form.save(commit=False)
-                form_2.instance = parent
-                form_2.save(commit=False)
+                parent = form.save()
+                entries = form_2.save(commit=False)
+                
+                for idx, entry in enumerate(entries, start=1):
+                    entry.transaction = parent
+                    entry.line_number = idx
+                    entry.save()
+
                 print(parent, form_2)
-        return redirect("moneypyle:journal")
-    else:
-        print("nope")
-        form = TransactionForm(prefix="trans")
-        form_2 = EntryFormSet(prefix="entries")
+            return redirect("moneypyle:journal")
         context = {
         "form": form,
         "form_2": form_2
         }      
-    return render( request, "transactions/journal.html", context=context)
+        return render( request, "transactions/journal.html", context=context)
+    form = TransactionForm(prefix="trans")
+    formset = EntryFormSet(prefix="entries")
+    context = {
+        "form": form,
+        "form_2": formset,
+    }
+    return render(request, "transactions/journal.html", context)
